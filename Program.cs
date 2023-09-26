da64c4d16ae3cfd7e3b5e2279aa465d8d5ee1251
VladimirKostylev <146061973+VladimirKostylev@users.noreply.github.com>
2023-09-26 10:14:26 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,9 +4,21 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int n = 1;
+            double S = 0;
+            double a = 0;
+            double e = 0.0001;
+            double x = 0;
+            do
+            {
+                a = Math.Cos(n * x) / Math.Pow(n, 2);
+                S += a;
+                n++;
+            }
+            while (Math.Abs(a) > e);
+            Console.WriteLine(S);
         }
     }
 }
