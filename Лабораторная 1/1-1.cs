d7e5496a426f9c283df0eef3be34b110bb6bfe13
VladimirKostylev <146061973+VladimirKostylev@users.noreply.github.com>
2023-10-03 10:01:13 +0300

    Add files via upload

---
+++ Лабораторная 1/1-1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Program
+{
+class Program
+{
+    static void Main()
+    {
+        int S=0;
+        for (int i=2;i<=35;i+=3)
+        {
+            S=S+i;
+        }
+        Console.WriteLine (S);
+    }
+}
+}
